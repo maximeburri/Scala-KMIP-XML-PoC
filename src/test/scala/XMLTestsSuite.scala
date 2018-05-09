ca4961b817a11cdc1e058e14edcf07d313650fbf
Maxime Burri <maxime.burri@gmail.com>
2018-05-09 13:59:36 +0200

    Add encoding/decoding XML tests

---
+++ src/test/scala/XMLTestsSuite.scala
@@ -0,0 +1,125 @@
+package KMIPTestsSuites
+
+import org.scalatest._
+import Matchers._
+
+import KMIP._
+import KMIP.XML._
+import org.scalatest.FunSuite
+
+class XMLTestsSuite  extends FunSuite{
+
+  val completeKeyBlock = KeyBlock(
+    KeyFormatType(
+      StringValue("X.509")
+    ),
+    Some(
+      KeyValue(
+        ByteStringValue(
+          List[Byte](0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9,
+            0xab.toByte, 0xcd.toByte, 0xef.toByte)
+        )
+      )
+    ),
+    CryptographicLength(
+      IntegerValue(16)
+    )
+  )
+
+  val keyBlockWithoutKeyValue = KeyBlock(
+    KeyFormatType(
+      StringValue("X.509")
+    ),
+    None,
+    CryptographicLength(
+      IntegerValue(16)
+    )
+  )
+
+  test("Decoding an XML KeyBlock should be a KeyBlock") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyFormatType type="String" value="X.509"/>
+      <KeyValue type="ByteString" value="00010203040506070809abcdef"/>
+      <CryptographicLength type="Integer" value="16"/>
+    </KeyBlock>
+
+    val result = fromXML(keyBlockXML)
+    result shouldBe a [KeyBlock]
+    result should be(completeKeyBlock)
+  }
+
+  test("Decoding an optional field should be accepted") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyFormatType type="String" value="X.509"/>
+      <CryptographicLength type="Integer" value="16"/>
+    </KeyBlock>
+
+    val result : KeyBlock = fromXML(keyBlockXML).asInstanceOf[KeyBlock]
+    result.keyValue should be (None)
+  }
+
+  test("Decoding a missing required field should fail") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyValue type="ByteString" value="00010203040506070809abcdef"/>
+      <CryptographicLength type="Integer" value="16"/>
+    </KeyBlock>
+
+    the [Exception] thrownBy fromXML(keyBlockXML) should have message
+      "Types not equal. Type expected : KMIP.KeyFormatType, type found : KMIP.KeyValue"
+  }
+
+  test("Decoding an XML with an unknown structure should fail") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyFormatType type="String" value="X.509"/>
+      <ACustomKeyValue type="ByteString" value="00010203040506070809abcdef"/>
+      <CryptographicLength type="Integer" value="16"/>
+    </KeyBlock>
+
+    the [Exception] thrownBy fromXML(keyBlockXML) should have message
+      "Node label ACustomKeyValue not recognized"
+  }
+
+  test("Decoding an XML with a bad type should fail") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyFormatType type="String" value="X.509"/>
+      <KeyFormatType type="String" value="X.509"/>
+      <CryptographicLength type="Integer" value="16"/>
+    </KeyBlock>
+
+    the [Exception] thrownBy fromXML(keyBlockXML) should have message
+      "Types not equal. Type expected : KMIP.CryptographicLength, type found : KMIP.KeyFormatType"
+  }
+
+  test("Decoding an XML with too many fields") {
+    val keyBlockXML = <KeyBlock type="Structure">
+      <KeyFormatType type="String" value="X.509"/>
+      <KeyValue type="ByteString" value="00010203040506070809abcdef"/>
+      <CryptographicLength type="Integer" value="16"/>
+      <CryptographicLength type="Integer" value="17"/>
+    </KeyBlock>
+
+    the [Exception] thrownBy fromXML(keyBlockXML) should have message
+      "Too much nodes for structure but found : List(<CryptographicLength type=\"Integer\" value=\"17\"/>, \n    )"
+  }
+
+  test("Encoding a complete KeyBlock should be a XML KeyBlock") {
+    val result = <KeyBlock type="Structure"><KeyFormatType type="String" value="X.509"/><KeyValue type="ByteString" value="00010203040506070809abcdef"/><CryptographicLength type="Integer" value="16"/></KeyBlock>
+
+    toXML(completeKeyBlock) should be (result)
+  }
+
+  test("Encoding a KeyBlock with missing optional field be a XML KeyBlock") {
+    val result = <KeyBlock type="Structure"><KeyFormatType type="String" value="X.509"/><CryptographicLength type="Integer" value="16"/></KeyBlock>
+
+    toXML(keyBlockWithoutKeyValue) should be (result)
+  }
+
+  test("Encoding and decoding a complete KeyBlock should be identical") {
+    fromXML(toXML(completeKeyBlock)) should be (completeKeyBlock)
+  }
+
+  test("Encoding and decoding a KeyBlock with optional field should be identical") {
+    fromXML(toXML(keyBlockWithoutKeyValue)) should be (keyBlockWithoutKeyValue)
+  }
+
+}
